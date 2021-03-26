36684e21bd334a91eaac2089327fd59b17d9af5b
syoung7388 <79610047+syoung7388@users.noreply.github.com>
2021-03-26 15:02:20 +0900

    11

---
+++ src/usedf/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+<v-app>
+<v-container>
+  <v-row style="height: 600px;"  align="center" justify="center">
+    <v-col style="weight: 600px;">
+      <h4 class="primary--text" style="font-size: 70px; text-align: center" >Used</h4>
+      <v-spacer></v-spacer>
+      <v-btn class="ml-16">로그인</v-btn>
+      <v-btn  class="ma-4">회원가입</v-btn>
+ 
+     </v-col>
+  </v-row>
+</v-container>
+</v-app>
+</template>
+
---
+++ src/usedf/src/plugins/vuetify.js
@@ -0,0 +1,19 @@
+import Vue from 'vue'
+import Vuetify from 'vuetify'
+
+
+Vue.use(Vuetify)
+
+export default new Vuetify({
+  theme: {
+    themes: {
+      light: {
+        primary: '#1182F3',
+        secondary: '#9E9E9E', 
+        error: '#F00000', 
+        green: '#31C923',
+        grey: '#E0E0E0'
+      },
+    },
+  },
+})
