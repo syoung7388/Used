c63a2ca51a1c477e1fce3fee25bc387838ae4c0b
syoung7388 <79610047+syoung7388@users.noreply.github.com>
2021-03-31 15:12:50 +0900

    Signup ok

--- db.sql
+++ db.sql
@@ -1,6 +1,6 @@
 -- --------------------------------------------------------
 -- 호스트:                          127.0.0.1
--- 서버 버전:                        10.5.4-MariaDB - mariadb.org binary distribution
+-- 서버 버전:                        10.5.9-MariaDB - mariadb.org binary distribution
 -- 서버 OS:                        Win64
 -- HeidiSQL 버전:                  11.0.0.5919
 -- --------------------------------------------------------
@@ -11,42 +11,27 @@
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 
+-- 테이블 used.user 구조 내보내기
+CREATE TABLE IF NOT EXISTS `user` (
+  `u_password` varchar(500) NOT NULL,
+  `u_username` varchar(50) NOT NULL,
+  `u_email` varchar(50) NOT NULL,
+  `u_address` varchar(50) NOT NULL,
+  `u_phone` varchar(50) NOT NULL,
+  `u_num` int(11) NOT NULL AUTO_INCREMENT,
+  `u_isAccountNonLocked` tinyint(4) DEFAULT NULL,
+  `u_isAccountNonExpired` tinyint(4) DEFAULT NULL,
+  `u_isCredentialsNonExpired` tinyint(4) DEFAULT NULL,
+  `u_isEnabled` tinyint(4) DEFAULT NULL,
+  PRIMARY KEY (`u_num`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
 
--- used 데이터베이스 구조 내보내기
-CREATE DATABASE IF NOT EXISTS `used` /*!40100 DEFAULT CHARACTER SET utf8 */;
-USE `used`;
-
--- 테이블 used.spring_session 구조 내보내기
-CREATE TABLE IF NOT EXISTS `spring_session` (
-  `PRIMARY_ID` char(36) NOT NULL,
-  `SESSION_ID` char(36) NOT NULL,
-  `CREATION_TIME` bigint(20) NOT NULL,
-  `LAST_ACCESS_TIME` bigint(20) NOT NULL,
-  `MAX_INACTIVE_INTERVAL` int(11) NOT NULL,
-  `EXPIRY_TIME` bigint(20) NOT NULL,
-  `PRINCIPAL_NAME` varchar(100) DEFAULT NULL,
-  PRIMARY KEY (`PRIMARY_ID`),
-  UNIQUE KEY `SPRING_SESSION_IX1` (`SESSION_ID`),
-  KEY `SPRING_SESSION_IX2` (`EXPIRY_TIME`),
-  KEY `SPRING_SESSION_IX3` (`PRINCIPAL_NAME`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC;
-
--- 테이블 데이터 used.spring_session:~0 rows (대략적) 내보내기
-/*!40000 ALTER TABLE `spring_session` DISABLE KEYS */;
-/*!40000 ALTER TABLE `spring_session` ENABLE KEYS */;
-
--- 테이블 used.spring_session_attributes 구조 내보내기
-CREATE TABLE IF NOT EXISTS `spring_session_attributes` (
-  `SESSION_PRIMARY_ID` char(36) NOT NULL,
-  `ATTRIBUTE_NAME` varchar(200) NOT NULL,
-  `ATTRIBUTE_BYTES` blob NOT NULL,
-  PRIMARY KEY (`SESSION_PRIMARY_ID`,`ATTRIBUTE_NAME`),
-  CONSTRAINT `SPRING_SESSION_ATTRIBUTES_FK` FOREIGN KEY (`SESSION_PRIMARY_ID`) REFERENCES `spring_session` (`PRIMARY_ID`) ON DELETE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC;
-
--- 테이블 데이터 used.spring_session_attributes:~0 rows (대략적) 내보내기
-/*!40000 ALTER TABLE `spring_session_attributes` DISABLE KEYS */;
-/*!40000 ALTER TABLE `spring_session_attributes` ENABLE KEYS */;
+-- 테이블 데이터 used.user:~2 rows (대략적) 내보내기
+/*!40000 ALTER TABLE `user` DISABLE KEYS */;
+INSERT INTO `user` (`u_password`, `u_username`, `u_email`, `u_address`, `u_phone`, `u_num`, `u_isAccountNonLocked`, `u_isAccountNonExpired`, `u_isCredentialsNonExpired`, `u_isEnabled`) VALUES
+	('$2a$10$keVqvWUo2WdnimiIM6V4EO8fT1TsMQMK.PXll2yKItydjPRwPacG6', '영이', 'dddd04032@naver.com', '대구 동구 신암로16길 25 (신천동, 신천자이아파트)', '01083747388', 1, 1, 1, 1, 1),
+	('$2a$10$8iao49vtikIwJyho5kI6VOvar7zgUkGpEBZK53UlzV9j1UfgF49Wy', 'a', 'aaa@aaa', '대구 북구 침산남로 1 (노원동1가)', '01083747388', 2, 1, 1, 1, 1);
+/*!40000 ALTER TABLE `user` ENABLE KEYS */;
 
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
 /*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
